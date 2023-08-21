1f21dd3b381ad5da60e0ac5c4ed8b90474de447e
harshnatuskar <harshnatuskar10@gmail.com>
2023-08-21 22:33:49 +0530

    styles and data added

---
+++ src/data/portfolio.ts
@@ -0,0 +1,22 @@
+export default[
+    {
+        title:'Command line tool',
+        imgUrl:'/assets/black.png',
+        stack:['HTML','CSS','Javascript']
+    },
+    {
+        title:'Calculator',
+        imgUrl:'/assets/black.png',
+        stack:['React']
+    },
+    {
+        title:'Markdown app',
+        imgUrl:'/assets/black.png',
+        stack:['React']
+    },
+    {
+        title:'Random Quote generator',
+        imgUrl:'/assets/black.png',
+        stack:['React']
+    },
+]
