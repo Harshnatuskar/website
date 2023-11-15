d9eb238758492b147e7e68773af58cc04c2d2390
harshnatuskar <harshnatuskar10@gmail.com>
2023-11-15 23:11:23 +0530

    added book reviews

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -6,7 +6,7 @@ import ui_showcase from '../data/ui_showcase';
 function Portfolio(){
     return (
         <div className="flex flex-col md:flex-row items-center justify-center">
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 '>
                 {portfolio.map(project => (
                     <PortfolioItem
                         imgUrl={project.imgUrl}
--- src/pages/Read.tsx
+++ src/pages/Read.tsx
@@ -4,30 +4,50 @@ function Read(){
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
         <h1 className='text-3xl md:text-7xl mb-1 mb:mb-3 font-medium dark:text-white'>Bookshelf</h1><br />
         <h2 className='text-base max-w-xl mb-6 font-bold'>Here are some books that i loved reading:</h2>
-          <section className="max-w-xl mx-auto "> 
- 
-            <h3>The Alamanac of Naval Ravikant </h3>
-            <h3>Mastery </h3>
-            <h3>If truth be told </h3> 
-            <h3>Courage is Calling</h3>
-            <h3>The Obstacle is the way</h3>
-            <h3>Atomic Habits  </h3>
-            <h3>Love yourself like your life depends on it</h3>
-            <h3>The 4-hour workweek  </h3>
-            <h3>The 4-hour body</h3>
-            <h3>Digital Minimalism</h3>
-            <h3>Tools of Titans  </h3>
-            <h3>The Name of the Wind  </h3>
-            <h3>The Wise Man's Fear</h3>
-            <h3>The Way of Kings</h3>
-            <h3>The Words of Radiance</h3>
-            <h3>OathBringer</h3>
-            <h3>Rhythm of War</h3>
-            <h3>Edgedancer</h3>
-            <h3>Mistborn Triology</h3>
-            <h3>Assasin's Apprentice</h3> 
-            <h3>War Breaker</h3>
-            <h3>Ocean at the end of the lane</h3>
+          <section className="max-w-xl mx-auto text-left">  
+             
+            <h3>⬥ The Alamanac of Naval Ravikant -Eric Jorgenson</h3>
+            <h4 className="ml-4">If i could only read a single non-fiction book for the rest of my life this would be it</h4>
+            <h3>⬥ Mastery -Robert Greene</h3>
+            <h4 className="ml-4">I go back to this book every month or so .Has stories about great people and how they autodidacted in their lives.</h4>
+            <h3>⬥ If truth be told -Om Swami</h3> 
+            <h4 className="ml-4">Mindblowing</h4>
+            <h3>⬥ Courage is Calling -Ryan Holiday</h3>
+            <h4 className="ml-4">"If we wish to be great , we must first learn how to conquer fear, or at least rise above it in the moments that matter" One of my favourate quotes in this book</h4>
+            <h3>⬥ The Obstacle is the way -Ryan Holiday</h3>
+            <h4 className="ml-4">When ever i feel stuck in life i go back to this book and read any one of its chapters</h4>
+            <h3>⬥ Atomic Habits  -James Clear</h3>
+            <h4 className="ml-4">All about habits in as simple words possible.</h4>
+            <h3>⬥ Love yourself like your life depends on it -Kamal Ravikant</h3>
+            <h4 className="ml-4">A short read , brutally honest book on how not to lose yourself in the chaos around you.</h4>
+            <h3>⬥ The 4-hour workweek -Tim Ferriss</h3>
+            <h4 className="ml-4">Tells you how not to waste your life and do what really matters.</h4> 
+            <h3>⬥ Digital Minimalism - Cal Newport</h3>
+            <h4 className="ml-4">You will use tech more intentionally after reading this one</h4>
+            <h3>⬥ Tools of Titans -Tim Ferriss</h3>
+            <h4 className="ml-4">Advice from worldclass athletes,writers,VC's you name it.</h4>
+            <h3>⬥ The Name of the Wind  -Patrick Rothfuss</h3>
+            <h4 className="ml-4">The prose , storyline and characters are so well crafted. I have read it multiple times and each time i find something new in it.</h4>
+            <h3>⬥ The Wise Man's Fear -Patrick Rothfuss</h3>
+            <h4 className="ml-4">2nd part of the king chronicles series, the story telling is as good as the first part.</h4>
+            <h3>⬥ Mistborn Triology -Brandon Sanderson</h3>
+            <h4 className="ml-4">This series reignited my love for reading fiction.</h4>
+            <h3>⬥ The Way of Kings -Brandon Sanderson</h3>
+            <h4 className="ml-4">The author throws you directly into the world, and 300 pages or so later everythings makes sense.</h4>
+            <h3>⬥ Words of Radiance -Brandon Sanderson</h3>
+            <h4 className="ml-4">Has so many character perspectives, the lore is immense, the characters are complex .You never get bored.</h4>
+            <h3>⬥ OathBringer -Brandon Sanderson</h3>
+            <h4 className="ml-4">Based over one of the best characters in the cosmere, Dalinar Kholin, amazing read.</h4>
+            <h3>⬥ Rhythm of War -Brandon Sanderson</h3>
+            <h4 className="ml-4">Has the best actions scenes in the entire cosmere.Immense character development.</h4>
+            <h3>⬥ Edgedancer -Brandon Sanderson</h3>
+            <h4 className="ml-4">A short read on side quests of a stormlight archive character.</h4>
+            <h3>⬥ Assasin's Apprentice -Robin Hobb</h3> 
+            <h4 className="ml-4">Robin Hobb is the best fpp writer, this books is the proof.</h4>
+            <h3>⬥ War Breaker -Brandon Sanderson</h3>
+            <h4 className="ml-4">About fantasy planet based upon the power of breaths.</h4>
+            <h3>⬥ Ocean at the end of the lane -Neil Gaiman</h3>
+            <h4 className="ml-4">Nostalgic </h4>
             <br />
              
             <h2 className='font-bold'>Here are some of my favourate essays:</h2>
