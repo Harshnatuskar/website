10adcb99d9728913bcca776f769d9c5c642f274b
harshnatuskar <harshnatuskar10@gmail.com>
2023-11-26 12:21:30 +0530

    string converted to array

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -3,13 +3,13 @@ export default[
         title:'Sakura Mochi',
         imgUrl:'/assets/black.png',
         stack:['JUCE' ],
-        link:['https://github.com/Harshnatuskar/sakura-mochi']
+        link:'https://github.com/Harshnatuskar/sakura-mochi'
     },
     {
         title:'Life in dots',
         imgUrl:'/assets/black.png',
         stack:['Svelte' ],
-        link:['https://life-in-dots-seven.vercel.app/']
+        link:'https://life-in-dots-seven.vercel.app/'
     },
 
 ]
--- src/data/ui_showcase.ts
+++ src/data/ui_showcase.ts
@@ -3,18 +3,18 @@ export default[
         title:'Weatherwiz',
         imgUrl:'/assets/black.png',
         stack:['Figma'],
-        link:['https://www.behance.net/gallery/166586541/Weatherwiz']
+        link:'https://www.behance.net/gallery/166586541/Weatherwiz'
     },
     {
         title:'Mech-keys',
         imgUrl:'/assets/black.png',
         stack:['Figma'],
-        link:['https://www.behance.net/gallery/156776209/mech-keys-ui']
+        link:'https://www.behance.net/gallery/156776209/mech-keys-ui'
     }, 
     {
         title:'Grand tour',
         imgUrl:'/assets/black.png',
         stack:['Figma'],
-        link:['https://www.behance.net/gallery/177448771/Grandtour']
+        link:'https://www.behance.net/gallery/177448771/Grandtour'
     }, 
 ]
