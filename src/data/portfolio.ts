b2110c69c73bcf093a54cc01172d5e39799a5efb
harshnatuskar <harshnatuskar10@gmail.com>
2023-12-20 21:13:24 +0530

    rearranged

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,15 +1,15 @@
 export default[ 
     {
-        title:'Sakura Mochi',
-        imgUrl:'/assets/black.png',
-        stack:['JUCE' ],
-        link:'https://github.com/Harshnatuskar/sakura-mochi'
-    },
-    {
         title:'Life in dots',
         imgUrl:'/assets/black.png',
         stack:['Svelte' ],
         link:'https://life-in-dots-seven.vercel.app/'
+    },
+    {
+        title:'Sakura Mochi',
+        imgUrl:'/assets/black.png',
+        stack:['JUCE' ],
+        link:'https://github.com/Harshnatuskar/sakura-mochi'
     },
 
 ]
